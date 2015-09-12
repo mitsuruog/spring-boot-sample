40faaee17b4a24087436d51be858bf82bec4d88c
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-09-13 00:12:34 +0900

    Create spring boot application

---
+++ src/main/java/jp/mitsuruog/springboot/sample/SpringBootSampleApplication.java
@@ -0,0 +1,14 @@
+package jp.mitsuruog.springboot.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBootSampleApplication.class, args);
+
+    }
+}
