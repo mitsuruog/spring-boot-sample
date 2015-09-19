c10fa3bf743d0c7b49a2504c536b353eafa76f2b
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-09-20 01:08:49 +0900

    H2データベースが呼び出せているか簡単なサンプルを追加

--- src/main/java/jp/mitsuruog/springboot/sample/Customer/App.java
+++ src/main/java/jp/mitsuruog/springboot/sample/Customer/App.java
@@ -7,6 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 /**
  * Created by mitsuruog on 15/09/19.
@@ -15,19 +19,29 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class App implements CommandLineRunner{
 
+//    @Autowired
+//    CustomerService customerService;
+
+    // autoconfigureという仕組みで依存関係を設定せずにSpring boot側で勝手にDIコンテナに登録されるみたい
     @Autowired
-    CustomerService customerService;
+    NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public void run(String... args) throws Exception {
 
-        // 追加
-        customerService.save(new Customer(1, "Mae", "Matthews"));
-        customerService.save(new Customer(2, "Antonio", "Carr"));
-        customerService.save(new Customer(3, "Ralph", "Henderson"));
+        String sql = "SELECT 1";
+        SqlParameterSource parameter = new MapSqlParameterSource();
+        // queryForObject->SQL, パラメータ, 戻り値のクラスを設定する
+        Integer result = jdbcTemplate.queryForObject(sql, parameter, Integer.class);
+        System.out.println("result -> " + result);
 
-        // データ表示
-        customerService.findAll().forEach((x) -> System.out.println(x));
+//        // 追加
+//        customerService.save(new Customer(1, "Mae", "Matthews"));
+//        customerService.save(new Customer(2, "Antonio", "Carr"));
+//        customerService.save(new Customer(3, "Ralph", "Henderson"));
+//
+//        // データ表示
+//        customerService.findAll().forEach((x) -> System.out.println(x));
 
     }
 
