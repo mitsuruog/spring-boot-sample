527a802917e745bc547b71884bbaeb9271ebfc68
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-09-20 01:33:34 +0900

    RowMapperを利用してDBの取得データをPOJOに置き換えるようにした

--- src/main/java/jp/mitsuruog/springboot/sample/Customer/App.java
+++ src/main/java/jp/mitsuruog/springboot/sample/Customer/App.java
@@ -7,10 +7,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by mitsuruog on 15/09/19.
@@ -29,12 +33,13 @@ public class App implements CommandLineRunner{
     @Override
     public void run(String... args) throws Exception {
 
-        String sql = "SELECT :a + :b";
+        String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
         SqlParameterSource parameter = new MapSqlParameterSource()
-                .addValue("a", 100)
-                .addValue("b", 200);
+                .addValue("id", 1);
         // queryForObject->SQL, パラメータ, 戻り値のクラスを設定する
-        Integer result = jdbcTemplate.queryForObject(sql, parameter, Integer.class);
+        Customer result = jdbcTemplate.queryForObject(sql, parameter, (rs, rowNum) ->
+                        new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"))
+        );
         System.out.println("result -> " + result);
 
     }
