ebd0e12cfc406d4043315518deddd4088fff1c9d
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-09-20 14:45:37 +0900

    NamedParameterJdbcTemplateを利用してDBを操作できるようにした

--- src/main/java/jp/mitsuruog/springboot/sample/Customer/App.java
+++ src/main/java/jp/mitsuruog/springboot/sample/Customer/App.java
@@ -7,14 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.SqlParameter;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 /**
  * Created by mitsuruog on 15/09/19.
@@ -23,24 +15,17 @@ import java.sql.SQLException;
 @ComponentScan
 public class App implements CommandLineRunner{
 
-//    @Autowired
-//    CustomerService customerService;
-
-    // autoconfigureという仕組みで依存関係を設定せずにSpring boot側で勝手にDIコンテナに登録されるみたい
     @Autowired
-    NamedParameterJdbcTemplate jdbcTemplate;
+    CustomerService customerService;
 
     @Override
     public void run(String... args) throws Exception {
 
-        String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
-        SqlParameterSource parameter = new MapSqlParameterSource()
-                .addValue("id", 1);
-        // queryForObject->SQL, パラメータ, 戻り値のクラスを設定する
-        Customer result = jdbcTemplate.queryForObject(sql, parameter, (rs, rowNum) ->
-                        new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"))
-        );
-        System.out.println("result -> " + result);
+        Customer created = customerService.save(new Customer(null, "mitsuru", "ogawa"));
+        System.out.println(created + "is created!!");
+
+        customerService.findAll().forEach(
+                (customer) -> System.out.println(customer));
 
     }
 
--- src/main/java/jp/mitsuruog/springboot/sample/Customer/repository/CustomerRepository.java
+++ src/main/java/jp/mitsuruog/springboot/sample/Customer/repository/CustomerRepository.java
@@ -1,12 +1,15 @@
 package jp.mitsuruog.springboot.sample.Customer.repository;
 
 import jp.mitsuruog.springboot.sample.Customer.domain.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 /**
  * Created by mitsuruog on 15/09/19.
@@ -14,23 +17,54 @@ import java.util.concurrent.ConcurrentMap;
 @Repository
 public class CustomerRepository {
 
-    // customerIdをkeyにしている
-    private final ConcurrentHashMap<Integer, Customer> customerMap = new ConcurrentHashMap<>();
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
+
+    private static final RowMapper<Customer> CUSTOMER_ROW_MAPPER = (rs, rowNum) -> {
+        return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
+    };
 
     public List<Customer> findAll() {
-        return new ArrayList<>(customerMap.values());
+        List<Customer> customers = jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers ORDER BY id",
+                CUSTOMER_ROW_MAPPER
+        );
+        return customers;
     }
 
     public Customer findById(Integer customerId) {
-        return customerMap.get(customerId);
+        SqlParameterSource parameterSource = new MapSqlParameterSource()
+                .addValue("id", customerId);
+        return jdbcTemplate.queryForObject(
+                "SELECT id, first_name, last_name FROM customers WHERE id = :id",
+                parameterSource,
+                CUSTOMER_ROW_MAPPER);
     }
 
     public Customer save(Customer customer) {
-        return customerMap.put(customer.getId(), customer);
+        // BeanPropertySqlParameterSourceはbeanの内容をパラメータにMapする便利なものらしい
+        SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(customer);
+        if(customer.getId() == null) {
+            jdbcTemplate.update(
+                  "INSERT INTO customers(first_name, last_name) VALUES (:firstName, :lastName)",
+                  parameterSource
+          );
+        } else {
+            jdbcTemplate.update(
+                    "UPDATE customers SET first_name = :firstName, last_name = :lastName WHERE id = :id",
+                    parameterSource
+            );
+        }
+        return customer;
     }
 
     public void delete(Integer customerId) {
-        customerMap.remove(customerId);
+        SqlParameterSource parameterSource = new MapSqlParameterSource()
+                .addValue("id", customerId);
+        jdbcTemplate.update(
+                "DELETE FROM customers WHERE id = :id",
+                parameterSource
+        );
     }
 
 }
