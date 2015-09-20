615bf1c8dd9bf8da35b6054b368cf558c7e5043a
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-09-20 14:57:20 +0900

    insert時に生成されたkeyが取得できていないので、insert後に取得できるよう変更した

--- src/main/java/jp/mitsuruog/springboot/sample/Customer/repository/CustomerRepository.java
+++ src/main/java/jp/mitsuruog/springboot/sample/Customer/repository/CustomerRepository.java
@@ -2,13 +2,16 @@ package jp.mitsuruog.springboot.sample.Customer.repository;
 
 import jp.mitsuruog.springboot.sample.Customer.domain.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 /**
@@ -19,6 +22,18 @@ public class CustomerRepository {
 
     @Autowired
     NamedParameterJdbcTemplate jdbcTemplate;
+
+    // 生成されたSQL文を保持する
+    SimpleJdbcInsert insert;
+
+    @PostConstruct
+    public void init() {
+        // SimpleJdbcInsertではinsert文を自動生成する
+        // なんで、最初からこれだとダメなのか？？
+        insert = new SimpleJdbcInsert((JdbcTemplate) jdbcTemplate.getJdbcOperations())
+                .withTableName("customers")
+                .usingGeneratedKeyColumns("id"); // これを実行すると自動採番されたIDが返される
+    }
 
     private static final RowMapper<Customer> CUSTOMER_ROW_MAPPER = (rs, rowNum) -> {
         return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
@@ -45,10 +60,9 @@ public class CustomerRepository {
         // BeanPropertySqlParameterSourceはbeanの内容をパラメータにMapする便利なものらしい
         SqlParameterSource parameterSource = new BeanPropertySqlParameterSource(customer);
         if(customer.getId() == null) {
-            jdbcTemplate.update(
-                  "INSERT INTO customers(first_name, last_name) VALUES (:firstName, :lastName)",
-                  parameterSource
-          );
+            // insert後のIDを取得する
+            Number key = insert.executeAndReturnKey(parameterSource);
+            customer.setId(key.intValue());
         } else {
             jdbcTemplate.update(
                     "UPDATE customers SET first_name = :firstName, last_name = :lastName WHERE id = :id",
