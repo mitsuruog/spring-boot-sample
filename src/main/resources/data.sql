4a365b6e952fad6259889ba3b1e29eb4bf1067b2
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-09-20 16:37:31 +0900

    JPAを利用する場合は、自動的に対応するテーブルのcreate tableが実行されるため、SQLからのテーブル作成は不要となった

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO customers(first_name, last_name) VALUES ('Mae', 'Matthews');
-INSERT INTO customers(first_name, last_name) VALUES ('Antonio', 'Carr');
-INSERT INTO customers(first_name, last_name) VALUES ('Ralph', 'Henderson');
-INSERT INTO customers(first_name, last_name) VALUES ('Ted', 'Fowler');
