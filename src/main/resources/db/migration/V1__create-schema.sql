5955c770f85035f0a3b120eaa4124b03a008d358
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-09-24 01:35:47 +0900

    flywayにUserテーブルスキーマを追加

--- src/main/resources/db/migration/V1__create-schema.sql
+++ src/main/resources/db/migration/V1__create-schema.sql
@@ -1,5 +1,11 @@
 CREATE TABLE customers (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
-    last_name VARCHAR(30)
+    last_name VARCHAR(30),
+    username VARCHAR(100) NOT NULL DEFAULT 'test'
+);
+
+CREATE TABLE users (
+    username VARCHAR(100) NOT NULL PRIMARY KEY,
+    encodedPassword VARCHAR(255)
 );
--- src/main/resources/db/migration/V2__import-initaial-data.sql
+++ src/main/resources/db/migration/V2__import-initaial-data.sql
@@ -2,3 +2,5 @@ INSERT INTO customers(first_name, last_name) VALUES ('Mae', 'Matthews');
 INSERT INTO customers(first_name, last_name) VALUES ('Antonio', 'Carr');
 INSERT INTO customers(first_name, last_name) VALUES ('Ralph', 'Henderson');
 INSERT INTO customers(first_name, last_name) VALUES ('Ted', 'Fowler');
+
+INSERT INTO users(username, encodedPassword) VALUES ('test', '$2a$10$Wbo2NXRjsKNlXVHW4OC8Qekl9t4ed24bdS4t/aJIBlPRR7IoblEvi');
