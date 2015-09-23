56741a99d01adee86f70dc30bf9f379381aa296b
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2015-09-23 22:31:40 +0900

    flyway用にスキーマ定義などをSQLとした

---
+++ src/main/resources/db/migration/V2__import-initaial-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers(first_name, last_name) VALUES ('Mae', 'Matthews');
+INSERT INTO customers(first_name, last_name) VALUES ('Antonio', 'Carr');
+INSERT INTO customers(first_name, last_name) VALUES ('Ralph', 'Henderson');
+INSERT INTO customers(first_name, last_name) VALUES ('Ted', 'Fowler');
